6b95b69b9c18f366ecfd9d9bf65dccc192702b78
Jaime Baro <jaimebaroval@gmail.com>
2020-04-29 22:49:32 +0200

    Interaction 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 // Iteration 1: Names and Input
-
-
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
+const hacker1 = 'Miguel';
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = 'Jaime';
+console.log(`The navigator's name is ${hacker2}`);
